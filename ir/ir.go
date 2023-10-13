88df5932b6925f5849f6e1bf42ef66d7f7a81fe4
Andrew Lewis <nerf@judo.za.org>
2023-10-13 13:32:03 +0200

    Add header to zonefiles; fix some funny stuff

--- ir/ir.go
+++ ir/ir.go
@@ -56,6 +56,9 @@ func ReadIRData(appCacheDir string, sources []string) (map[uint32]IRASNInfo, err
 			}
 
 			if entType == "asn" {
+				if entCountry == "" {
+					entCountry = "--"
+				}
 				irInfo := &IRASNInfo{
 					Country: entCountry,
 					IR:      IRID(entIR),
--- zonefile/zonefile.go
+++ zonefile/zonefile.go
@@ -8,6 +8,12 @@ import (
 	"github.com/rspamd/goasn/log"
 )
 
+const (
+	zoneHeader = `$SOA 43200 asn-ns.rspamd.com support.rspamd.com 0 600 300 86400 300
+$NS  43200 asn-ns.rspamd.com asn-ns2.rspamd.com
+`
+)
+
 func GenerateZone(asnToIRInfo map[uint32]ir.IRASNInfo, prefixToAS map[string]uint32, zonev4 string) error {
 
 	if zonev4 == "" {
@@ -19,6 +25,7 @@ func GenerateZone(asnToIRInfo map[uint32]ir.IRASNInfo, prefixToAS map[string]uin
 	if err != nil {
 		return fmt.Errorf("failed to create zonefile: %v", err)
 	}
+	fmt.Fprint(f, zoneHeader)
 	emptyInfo := ir.IRASNInfo{
 		IR:      ir.UNKNOWN,
 		Country: "--", // FIXME: types
@@ -27,10 +34,6 @@ func GenerateZone(asnToIRInfo map[uint32]ir.IRASNInfo, prefixToAS map[string]uin
 		irInfo, ok := asnToIRInfo[asnNo]
 		if !ok {
 			irInfo = emptyInfo
-		}
-		// WTF FIXME ???
-		if irInfo.Country == "" {
-			irInfo.Country = "--"
 		}
 		fmt.Fprintf(f, "%s %d|%s|%s|%s|\n", prefix, asnNo, prefix, irInfo.Country, irInfo.IR)
 	}
